6332ef314a31d457ab50a9cf27644e600ad08568
Simon Smith <smithsps@gmail.com>
2017-08-23 21:02:42 -0400

    Player stat reading completed.

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use std::io::prelude::*;
 use std::str::from_utf8;
 use nom::*;
 
-static REPLAY_FILE_STR: &'static str = "DB55DE0C49529D55D2D1E0B024869CF3.replay";
-//static REPLAY_FILE_STR: &'static str = "F32599A54B1831A58C6C55A5334890AF.replay";
+static REPLAY_FILE_STR: &'static str = "replays/DB55DE0C49529D55D2D1E0B024869CF3.replay";
+//static REPLAY_FILE_STR: &'static str = "replays/F32599A54B1831A58C6C55A5334890AF.replay";
 const DEFAULT_REPLAY_BUFFER: usize = 5400; // 2MB
 
 /*
@@ -58,12 +58,18 @@ named!(str_property<&str>,
         tag!(b"\x0C\x00\x00\x00StrProperty\0") >>
         property_size: le_u64 >>
         length: le_u32 >>
-        string: map_res!(
-            take!(length - 1), 
-            from_utf8
-        ) >>
+        string: take_str!(length - 1) >>
         null_terminator: take!(1) >>
         (string)
+    )
+);
+
+named!(bool_property<bool>,
+    do_parse!(
+        tag!(b"\x0D\x00\x00\x00BoolProperty\0") >>
+        boolean: le_u64 >>
+        null_terminator: take!(1) >>
+        (boolean > 0)
     )
 );
 
@@ -72,15 +78,37 @@ named!(name_property<&str>,
         tag!(b"\x0D\x00\x00\x00NameProperty\0") >>
         property_size: le_u64 >>
         length: le_u32 >>
-        string: map_res!(
-            take!(length - 1), 
-            from_utf8
-        ) >>
+        string: take_str!(length - 1) >>
         null_terminator: take!(1) >>
         (string)
     )
 );
 
+named!(byte_property,
+    do_parse!(
+        tag!(b"\x0D\x00\x00\x00ByteProperty\0") >>
+        bytes: take!(8) >>
+        (bytes)
+    )
+);
+
+named!(qword_property,
+    do_parse!(
+        tag!(b"\x0E\x00\x00\x00QWordProperty\0") >>
+        property_size: le_u64 >>
+        bytes: take!(8) >>
+        (bytes)
+    )
+);
+
+named!(raw_string<&str>,
+    do_parse!(
+        length: le_u32 >>
+        string: take_str!(length - 1) >>
+        null_terminator: take!(1) >>
+        (string) 
+    )
+);
 
 
 
@@ -140,14 +168,14 @@ named!(highlight_array <Vec<Highlight>>,
         str_HighLights >>
         array_size: array_property >>
 
-        highlights: length_count!(le_u32, dbg!(do_parse!(
+        highlights: length_count!(le_u32, do_parse!(
             str_frame >>
             frame: int_property >>
 
-            dbg!(str_CarName) >>
+            str_CarName >>
             car_name: name_property >>
 
-            dbg!(str_BallName) >>
+            str_BallName >>
             ball_name: name_property >>
             
             // Unknown why there is a 'None' delimiting
@@ -158,13 +186,111 @@ named!(highlight_array <Vec<Highlight>>,
                 car_name: car_name,
                 ball_name: ball_name
             })
-        ))) >>
+        )) >>
         (highlights)
     )
 );
 
+#[derive(Debug)]
+struct PlayerStats<'a> {
+    name: &'a str,
+    is_bot: bool,
+    platform: &'a [u8],
+    online_platform: &'a str,
+    online_id: &'a [u8],
+    team: u32,
+    score: u32,
+    goals: u32,
+    assists: u32,
+    saves: u32,
+    shots: u32
+}
 
-named!(get_header<(u32, u32, Vec<Goal>, Vec<Highlight>)>,
+named!(playerstats_array <Vec<PlayerStats>>,
+    do_parse!(
+        tag!(b"\x0C\x00\x00\x00PlayerStats\0") >>
+        array_size: array_property >>
+
+        stats: length_count!(le_u32, do_parse!(
+            tag!(b"\x05\x00\x00\x00Name\0") >>
+            player_name: str_property >>
+
+            tag!(b"\x09\x00\x00\x00Platform\0") >>
+            platform: byte_property >>
+
+            tag!(b"\x0F\x00\x00\x00OnlinePlatform\0") >>
+            online_platform: raw_string >>
+
+            tag!(b"\x09\x00\x00\x00OnlineID\0") >>
+            online_id: qword_property >>
+
+            tag!(b"\x05\x00\x00\x00Team\0") >>
+            team: int_property >>
+
+            tag!(b"\x06\x00\x00\x00Score\0") >>
+            score: int_property >>
+
+            tag!(b"\x06\x00\x00\x00Goals\0") >>
+            goals: int_property >>
+
+            tag!(b"\x08\x00\x00\x00Assists\0") >>
+            assists: int_property >>
+
+            tag!(b"\x06\x00\x00\x00Saves\0") >>
+            saves: int_property >>
+
+            tag!(b"\x06\x00\x00\x00Shots\0") >>
+            shots: int_property >>
+
+            tag!(b"\x05\x00\x00\x00bBot\0") >>
+            is_bot: bool_property >>
+
+            str_None >>
+
+            (PlayerStats {
+                name: player_name,
+                is_bot: is_bot,
+                platform: platform,
+                online_platform: online_platform,
+                online_id: online_id,
+                team: team,
+                score: score,
+                goals: goals,
+                assists: assists,
+                saves: saves,
+                shots: shots
+            })
+        )) >>
+        (stats)
+    )
+);
+
+#[derive(Debug)]
+struct MetaInfo<'a> {
+    replay_version: u32,
+    game_version: u32,
+    build_id: u32,
+    changelist: u32,
+    build_version: &'a str,
+    record_fps: f32,
+    keyframe_delay: f32,
+    max_channels: u32,
+    max_replay_size_mb: u32,
+    id: &'a str,
+    map_name: &'a str,
+    date: &'a str,
+    num_frames: u32,
+    match_type: &'a str,
+    online_player_name: &'a str,
+}
+
+named!(meta_info <Vec<MetaInfo>>,
+    do_parse!(
+        rest: rest >>
+    )
+);
+
+named!(get_header<(Vec<PlayerStats>)>,
     do_parse!(
         crc: take!(4) >>
         version_major: le_u32 >>
@@ -187,8 +313,10 @@ named!(get_header<(u32, u32, Vec<Goal>, Vec<Highlight>)>,
 
         highlights: highlight_array >>
 
+        stats: playerstats_array >>
+
         rest: rest >>
-        (team_0_score, team_1_score, goals, highlights)
+        (stats)
     )
 );
 
