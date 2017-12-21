d139892f271c6b260af837ff2321aecf7272e93a
Simon Smith <smithsps@gmail.com>
2017-12-21 02:33:13 -0500

    Proper CRC working (Atleast for Header).

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::io::prelude::*;
 use nom::{le_u32};
 
 mod header;
+mod crc32_rl;
 
 static REPLAY_FILE_STR: &'static str = "replays/1EBA9EA845DB4BD7809E78A7F4A7F1EC.replay";
 //static REPLAY_FILE_STR: &'static str = "replays/F32599A54B1831A58C6C55A5334890AF.replay";
@@ -26,13 +27,16 @@ fn parse(file: &str) -> io::Result<()> {
     //https://github.com/gcc-mirror/gcc/blob/master/libiberty/crc32.c
     let mut crc_buf = [0; 4];
     file.read_exact(&mut crc_buf);
-    let (_, crc) = le_u32(&mut hlen_buf).unwrap();
+    let (_, crc) = le_u32(&mut crc_buf).unwrap();
 
 
-    println!("{:?}", header_length);
+    println!("Header Length: {:?}\n", header_length);
 
     let mut h_buf = BufReader::with_capacity(header_length as usize, file);
     let h_bytes = h_buf.fill_buf()?;
+
+    print!("{:?}\n", crc32_rl::checksum(h_bytes));
+    print!("{:?}\n", crc);
 
     let header = header::get_header(h_bytes).to_result().unwrap();
     println!("{:?}", header);
