9978c8236f2e468a3d397f958d2334fd83b0b968
Simon Smith <smithsps@gmail.com>
2017-12-21 02:32:54 -0500

    Readme UPDATE, w/o Github flavoring yet.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ use std::io;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
-use nom::*;
+use nom::{le_u32};
 
 mod header;
 
-//static REPLAY_FILE_STR: &'static str = "replays/1EBA9EA845DB4BD7809E78A7F4A7F1EC.replay";
-static REPLAY_FILE_STR: &'static str = "replays/F32599A54B1831A58C6C55A5334890AF.replay";
+static REPLAY_FILE_STR: &'static str = "replays/1EBA9EA845DB4BD7809E78A7F4A7F1EC.replay";
+//static REPLAY_FILE_STR: &'static str = "replays/F32599A54B1831A58C6C55A5334890AF.replay";
 const DEFAULT_REPLAY_BUFFER: usize = 5400;
 
 
@@ -23,8 +23,11 @@ fn parse(file: &str) -> io::Result<()> {
     let (_, header_length) = le_u32(&mut hlen_buf).unwrap();
     
     //header_length += 4; // CRC is not initially included. 
-    let mut replay_crc = [0; 4];
-    file.read_exact(&mut replay_crc);
+    //https://github.com/gcc-mirror/gcc/blob/master/libiberty/crc32.c
+    let mut crc_buf = [0; 4];
+    file.read_exact(&mut crc_buf);
+    let (_, crc) = le_u32(&mut hlen_buf).unwrap();
+
 
     println!("{:?}", header_length);
 
